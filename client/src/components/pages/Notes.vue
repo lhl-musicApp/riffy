3a2c1a2ed598e98a8ff02e1108206af31efc7663
johnkmeas <johnkmeas@gmail.com>
2017-02-27 18:52:42 -0800

    added input to filter data in view/notes.vue

--- client/src/components/pages/Notes.vue
+++ client/src/components/pages/Notes.vue
@@ -17,25 +17,61 @@
       <button type="submit" class="btn btn-primary">Create Item</button>
       <hr>
     </form>
-<div class="row">
-  <div class="col-sm-6 col-md-4" v-for="note in notes">
-    <div class="thumbnail">
-      <img src="" alt="">
-      <div class="caption">
-        <img src="http://www.fillmurray.com/200/200" alt="">
-        <h3>{{ note.first_name }} {{ note.last_name }}</h3>
-        <ul>
-          <li>{{ note.id }}</li>
-          <li>{{ note.user_city }}</li>
-          <li>{{ note.user_country }}</li>
-        </ul>
 
-
-        <!-- <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p> -->
-      </div>
-    </div>
+<form id="main" v-cloak>
+  <div class="bar">
+      <!-- Create a binding between the searchString model and the text field -->
+    <input type="text" v-model="searchString" placeholder="Enter your search terms" />
   </div>
-</div>
+
+  <ul>
+        <!-- Render a li element for every entry in the computed filteredArticles array. -->
+    <li v-for="article in filteredArticles">
+      <div class="row">
+        <div class="col-sm-6 col-md-4" v-for="note in filteredArticles">
+          <div class="thumbnail">
+            <img src="" alt="">
+            <div class="caption">
+              <img src="http://www.fillmurray.com/200/200" alt="">
+              <h3>{{ note.first_name }} {{ note.last_name }}</h3>
+              <ul>
+                <li>{{ note.id }}</li>
+                <li>{{ note.user_city }}</li>
+                <li>{{ note.user_country }}</li>
+              </ul>
+
+
+              <!-- <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p> -->
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- <a v-bind:href="article.url"><img v-bind:src="article.image" /></a>
+      <p>{{article.title}}</p> -->
+    </li>
+  </ul>
+
+</form>
+
+ <!--    <div class="row">
+      <div class="col-sm-6 col-md-4" v-for="note in notes">
+        <div class="thumbnail">
+          <img src="" alt="">
+          <div class="caption">
+            <img src="http://www.fillmurray.com/200/200" alt="">
+            <h3>{{ note.first_name }} {{ note.last_name }}</h3>
+            <ul>
+              <li>{{ note.id }}</li>
+              <li>{{ note.user_city }}</li>
+              <li>{{ note.user_country }}</li>
+            </ul>
+
+
+            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+          </div>
+        </div>
+      </div>
+    </div> -->
 <!--     <table class="table">
       <thead class="thead thead-inverse">
         <tr>
@@ -72,6 +108,7 @@ import auth from '../../auth.js'
 export default {
   data () {
     return {
+      searchString: "",
       user: auth.user,
       error: null,
       notes: [],
@@ -86,7 +123,31 @@ export default {
       this.notes = response.data;
     });
   },
-  computed: {},
+  computed: {
+ // A computed property that holds only those articles that match the searchString.
+        filteredArticles: function () {
+            var articles_array = this.notes,
+                searchString = this.searchString;
+
+            if(!searchString){
+                return articles_array;
+            }
+
+            searchString = searchString.trim().toLowerCase();
+
+            articles_array = articles_array.filter(function(item){
+                if(item.first_name.toLowerCase().indexOf(searchString) !== -1){
+                    return item;
+                }
+                if(item.first_name.toLowerCase().indexOf(searchString) !== -1){
+                    return item;
+                }
+            })
+
+            // Return an array with the filtered data.
+            return articles_array;;
+        }
+  },
   methods: {
     remove (id) {
       this.$http.delete('notes/' + id).then(response => {
