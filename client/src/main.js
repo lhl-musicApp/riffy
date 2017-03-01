cdaa16c4fa1609983e5822195eb6122be63558bc
johnkmeas <johnkmeas@gmail.com>
2017-02-28 21:31:17 -0800

    refactoring no major change

--- client/src/main.js
+++ client/src/main.js
@@ -40,7 +40,7 @@ export var router = new Router({
 		{ path: '/reset/:token', component: ResetPassword },
 		{ path: '/verify/:id/:token', component: Verify },
 		{ name: 'noteSingle', path: '/notes/:id', component: NoteSingle },
-    { name: 'userSingle', path: '/users/:id', component: UserSingle, meta: { requiresAuth: true } }
+    { name: '/userSingle', path: '/users/:id', component: UserSingle, meta: { requiresAuth: true } }
 	]
 });
 
