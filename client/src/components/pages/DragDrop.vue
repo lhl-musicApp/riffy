35b5ed6884b86b7d0324d9c86bd6ba14afb299e1
johnkmeas <johnkmeas@gmail.com>
2017-03-02 12:27:55 -0800

    created dragdrop.vue

---
+++ client/src/components/pages/DragDrop.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div>
+    <h1>Drag and Drop Page</h1>
+  </div>
+</template>
+
+<script>
+import auth from '../../auth.js'
+export default {
+  data () {
+    return {
+      drop: {}
+
+    };
+  },
+  created () {
+    // this.$http.get('main').then(response => {
+    //   this.main = response.data;
+    // });
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  components: {}
+};
+</script>
+
+<style lang="css">
+</style>
