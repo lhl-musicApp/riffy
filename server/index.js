340c110f5b43e0502d10708634033d84c2957a09
johnkmeas <johnkmeas@gmail.com>
2017-03-02 09:58:04 -0800

    fixed bug in get router for bands in index.js

--- server/index.js
+++ server/index.js
@@ -539,9 +539,6 @@ router.get('/bands/:band_id', ejwt({
       // if (param_id === banddata.id) {
       //   console.log(data);
         res.json(data);
-      } else {
-        res.status(400).redirect('/index')
-      }
     })
     .catch((err) => {
       res.status(400).json(err);
