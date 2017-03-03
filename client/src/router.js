6504dc5b9b9afa9ccab1e5a322e8a68bd6b6cef3
Dustin OBrien <dcobrien82@gmail.com>
2017-03-03 23:05:06 +0000

    added userskills route path

--- client/src/router.js
+++ client/src/router.js
@@ -12,7 +12,7 @@ export var router = new Router({
 		{ path: '/verify/:id/:token', component: Verify },
 		{ name: 'noteSingle', path: '/notes/:id', component: NoteSingle },
     { name: 'userSingle', path: '/users/:id', component: UserSingle, meta: { checksAuth: true } },
-    // { name: 'userEditor', path: '/edituser', component: UserEditor, meta: { checksAuth: true } }
+		{ path: '/userskills/:id', component: UserSingle, meta: { checksAuth: true }},
     { path: '/bands', component: Bands, meta: { requiresAuth: true }},
     { name: 'bandSingle', path: '/bands/:id', component: BandSingle, meta: { checksAuth: true } },
     { path: '/bandtracks/:id', component: BandSingle, meta: { checksAuth: true }},
