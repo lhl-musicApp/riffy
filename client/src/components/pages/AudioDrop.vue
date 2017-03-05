3a986288f36efc8ce80c5973b3869ad0ee13412d
johnkmeas <johnkmeas@gmail.com>
2017-03-05 02:59:09 -0800

    implemented audio playback functionality

--- client/src/components/pages/AudioDrop.vue
+++ client/src/components/pages/AudioDrop.vue
@@ -11,15 +11,20 @@
       <button @click="removeAudio">Remove Audio</button>
       <button @click="saveAudio">Save Audio</button>
     </div>
+    <button @click="loadSound">loadsong</button>
+
   </div>
 </template>
+<script>
 
+</script>
 <script>
 import auth from '../../auth.js'
 export default {
   data () {
     return {
-      audio: ''
+      // audio: 'http://localhost:3000/uploads/out.mp3'
+      audio : ''
 
     };
   },
@@ -65,7 +70,29 @@ export default {
         .then(response => {
         console.log('saveAudi =>', response)
       });
+    },
+    loadSound: function () {
+      window.AudioContext = window.AudioContext||window.webkitAudioContext;
+      const context = new AudioContext();
+      const request = new XMLHttpRequest();
+      request.open("GET", "http://localhost:3000/uploads/out.mp3", true);
+      request.responseType = "arraybuffer";
+
+      request.onload = function() {
+      var Data = request.response;
+      process(Data);
+
+      function process(Data) {
+        const source = context.createBufferSource(); // Create Sound Source
+        context.decodeAudioData(Data, function(buffer){
+          source.buffer = buffer;
+          source.connect(context.destination);
+          source.start(context.currentTime);
+        })
+      }
     }
+  request.send();
+ }
   },
   components: {}
 };
