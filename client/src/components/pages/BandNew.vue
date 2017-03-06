a66512778a27ad5f5f4fb339e6010c5e79c66067
ylhoony <yl.hoony@gmail.com>
2017-03-06 00:27:03 +0000

    added function to create new band

---
+++ client/src/components/pages/BandNew.vue
@@ -0,0 +1,87 @@
+<template lang="html">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <form id="registration" v-on:submit.prevent="submit">
+          <div class="registration form-group">
+            <label for="band_name">Band Name</label>
+            <input v-model="band.band_name" placeholder="Band Name">
+            <br>
+            <label for="band_bio">Band Bio</label>
+            <input v-model="band.band_bio" placeholder="Band Bio">
+            <br>
+            <label for="band_influence">Influence</label>
+            <input v-model="band.band_influence" placeholder="Influenced by">
+            <br>
+            <label for="soundcloud_link">Page Link</label>
+            <input v-model="band.soundcloud_link" placeholder="Link to Youtube">
+            <br>
+            <label for="is_new_band">creating new band?</label>
+            <input v-model="band.is_new_band" placeholder="y / n">
+            <br>
+            <label for="has_open_position">has open position?</label>
+            <input v-model="band.has_open_position" placeholder="y / n">
+            <br>
+            <label for="looking_for">what positions are you looking for?</label>
+            <input v-model="band.looking_for" placeholder="positions">
+            <br>
+            <button form="registration" name="registration" type="submit">Save</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      error: null,
+      band: {
+        band_name: '',
+        band_bio: '',
+        band_influence: '',
+        soundcloud_link: '',
+        is_new_band: true,
+        has_open_position: false,
+        looking_for: ''
+      }
+      // ,
+      // band_user: {
+      //   user_id: localStorage.user_id,
+      //   isBandAdmin: true
+      // }
+    };
+  },
+
+  created () {
+    // console.log('this.$route.params.id: ', this.$route.params.id)
+    // console.log('localStorage.user_id: ', localStorage.user_id)
+    // this.$http.get('users/' + this.$route.params.id).then(response => {
+    //   this.user = response.data[0];
+    // })
+  },
+  computed: {
+
+  },
+  methods: {
+    submit(){
+      this.$http.post('bands/new', this.band)
+        .then(response => {
+          console.log(response);
+          const newBandId = response.body.band_id
+        // this.user = response.body;
+          console.log('this $route: ', this.$route);
+          console.log('this $router: ', this.$router);
+          this.$router.push('/');
+      });
+    }
+  },
+  components: {}
+};
+
+</script>
+
+<style lang="css">
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -21,6 +21,7 @@ import Search from './components/pages/Search.vue';
 import ForgotPassword from './components/pages/ForgotPassword.vue';
 import ResetPassword from './components/pages/ResetPassword.vue';
 import Bands from './components/pages/Bands.vue';
+import BandNew from './components/pages/BandNew.vue';
 import BandSingle from './components/pages/BandSingle.vue';
 import Tracks from './components/pages/Tracks.vue';
 import TrackSingle from './components/pages/TrackSingle.vue';
@@ -51,8 +52,8 @@ export var router = new Router({
 		{ path: '/verify/:id/:token', component: Verify },
 		{ name: 'noteSingle', path: '/notes/:id', component: NoteSingle },
     { name: 'userSingle', path: '/users/:id', component: UserSingle, meta: { requiresAuth: true } },
-		// { name: 'userEditor', path: '/edituser', component: UserSingle, meta: { requiresAuth: true } }
     { path: '/bands', component: Bands, meta: { requiresAuth: true } },
+    { path: '/bands/new', component: BandNew, meta: { requiresAuth: true } },
     { name: 'bandSingle', path: '/bands/:id', component: BandSingle, meta: { requiresAuth: true } },
     { path: '/bandtracks/:id', component: BandSingle, meta: { checksAuth: true }},
     { path: '/tracks', component: Tracks, meta: { requiresAuth: true } },
