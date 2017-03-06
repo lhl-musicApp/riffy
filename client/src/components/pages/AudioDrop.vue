9c175178779ed50f6a37ef0459b0f18ade1fe505
johnkmeas <johnkmeas@gmail.com>
2017-03-05 23:37:15 -0800

    modified audiodrop.vue to play audio from dynmaic link

--- client/src/components/pages/AudioDrop.vue
+++ client/src/components/pages/AudioDrop.vue
@@ -1,8 +1,8 @@
 <template lang="html">
   <div>
-    <h1>Audio Upload</h1>
+    <h1>Play Audio</h1>
     <div v-if="!audio">
-      <h2>Select audio file to up load</h2>
+      <h6>Select audio file to up load</h6>
       <input type="file" @change="onFileAudio">
     </div>
     <div v-else>
@@ -22,15 +22,12 @@ import auth from '../../auth.js'
 export default {
   data () {
     return {
-      // audio: 'http://localhost:3000/uploads/out.mp3'
       audio : ''
 
     };
   },
   created () {
-    // this.$http.get('main').then(response => {
-    //   this.main = response.data;
-    // });
+
   },
   computed: {
 
@@ -49,8 +46,7 @@ export default {
       reader.onload = function(event) {
 
         vm.audio = event.target.result.split(',');
-         var decodedImageData = btoa(vm.audio[1]);                    // the actual conversion of data from binary to base64 format
-        // callback(decodedImageData);
+         var decodedImageData = btoa(vm.audio[1]);
         return decodedImageData
       };
       reader.readAsDataURL(audioFile);
@@ -71,22 +67,24 @@ export default {
       });
     },
     loadSound: function () {
+      console.log('from audio this.$route.params.id:', this.$route.params.id)
       window.AudioContext = window.AudioContext||window.webkitAudioContext;
       const context = new AudioContext();
       const request = new XMLHttpRequest();
-      request.open("GET", "http://localhost:3000/uploads/out.mp3", true);
+      request.open('GET', 'http://localhost:3000/uploads/audio-' + this.$route.params.id +'.mp3', true);
       request.responseType = "arraybuffer";
 
       request.onload = function() {
       var Data = request.response;
       process(Data);
-
+      const source = context.createBufferSource();
       function process(Data) {
-        const source = context.createBufferSource(); // Create Sound Source
+        // const source = context.createBufferSource(); // Create Sound Source
         context.decodeAudioData(Data, function(buffer){
           source.buffer = buffer;
           source.connect(context.destination);
           source.start(context.currentTime);
+          source.stop(context.currentTime + 10);
         })
       }
     }
