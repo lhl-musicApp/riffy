2729a1249f1ac6040d797a2369f0338e3961e944
johnkmeas <johnkmeas@gmail.com>
2017-03-07 19:07:20 -0800

    modified mainpage so it displays only user with track_links and plays a their track when button is click

--- client/src/components/pages/Main.vue
+++ client/src/components/pages/Main.vue
@@ -25,7 +25,7 @@
         <button type="submit" class="btn btn-primary">Create Item</button>
         <hr>
       </form> -->
-    <audio-player></audio-player>
+
     <form id="main" v-cloak v-on:submit.prevent="submit">
       <div class="bar">
           <!-- Create a binding between the searchString model and the text field -->
@@ -38,20 +38,28 @@
           <div class="row">
             <div class="col-sm-6 col-md-4" v-for="main in filteredArticles">
               <div class="thumbnail">
-              <audio-player></audio-player>
-                <img src="" alt="">
-                <div class="caption">
-                  <img src="http://www.fillmurray.com/200/200" alt="">
+              <!-- <audio-player></audio-player> -->
+
+                <div class="caption user-container">
+                  <img :src="main.image_link">
+
                   <h3>{{ main.first_name }} {{ main.last_name }}</h3>
                   <ul>
                     <li><a :href="'users/' + main.id">{{ main.id }}</a></li>
                     <li>{{ main.user_city }}</li>
                     <li>{{ main.user_country }}</li>
-                    <li>{{ main.user_influence }}</li>
-                    <li>{{ main.isAvailable }}</li>
-                    <li>{{ main.looking_for }}</li>
+                    <li>{{ main.image_link }}</li>
+                    <li>{{ main.track_link}}</li>
                   </ul>
+                  <!-- This is the audio component -->
+                  <div>
+                    <h1>Play Audio</h1>
 
+                      <!-- <img :src="image" /> -->
+
+<!-- <button  v-on:click="playSound('hi')" >hi</button> -->
+                    <button id="magic-click" v-on:click="playSound(main.track_link)">Play</button>
+                  </div>
 
                   <!-- <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p> -->
                 </div>
@@ -67,7 +75,9 @@
 
 <script>
 import auth from '../../auth.js'
-import AudioPlayer from './AudioDrop.vue';
+// import AudioPlayer from './AudioDrop.vue';
+const context = new AudioContext();
+var source = null;
 // import vSelect from "vue-select"
 export default {
   // components: {vSelect},
@@ -81,9 +91,8 @@ export default {
         title: '',
         text: '',
       },
-      audio: [
-        // 'http://localhost:3000/uploads/audio-' + this.$route.params.id +'.mp3',
-      ]
+      audio : '',
+      trackname : ''
     };
   },
   created () {
@@ -94,7 +103,7 @@ export default {
   computed: {
  // A computed property that holds only those articles that match the searchString.
     filteredArticles: function () {
-
+// TODO Fix later, this generates duplicate data
       var articles_array = this.main,
       searchString = this.searchString;
       var stringArray = searchString.split(' ');
@@ -102,7 +111,7 @@ export default {
       var searchArray = [];
 
       if(!searchString){
-        return articles_array;
+        return articles_array = this.main;
       }
       searchString = searchString.trim().toLowerCase();
       console.log("searchString: ", searchString)
@@ -111,12 +120,8 @@ export default {
       articles_array = articles_array.filter(function(item){
         for (var key in item) {
             // console.log('obj[key]', typeof(item[key]));
-          if(typeof(item[key]) == typeof('string')){
+          if(typeof(item[key]) === typeof('string')){
             if(item[key].toLowerCase().indexOf(stringArray[0]) !== -1){
-              console.log('item=>', item)
-              return loadArray.push(item);
-            }
-            if(item[key].toLowerCase().indexOf(stringArray[1]) !== -1){
               console.log('item=>', item)
               return loadArray.push(item);
             }
@@ -151,13 +156,108 @@ export default {
           console.log(error);
         });
       }
-    }
+    },
+    onFileAudio(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length)
+        return;
+      this.createAudio(files[0]);
+    },
+    createAudio(audioFile) {
+      var reader = new FileReader();
+      var vm = this;
+      reader.onload = function(event) {
+
+        vm.audio = event.target.result.split(',');
+         var decodedImageData = btoa(vm.audio[1]);
+        return decodedImageData
+      };
+      reader.readAsDataURL(audioFile);
+      console.log(reader);
+    },
+    removeAudio: function (e) {
+      this.audio = '';
+    },
+    saveAudio: function (e) {
+      const audioObj = {
+        audio : this.audio,
+        trackname : this.trackname
+      }
+      // console.log('reader', this.image)
+      this.$http.post('upload/audio', { audioObj })
+        .then(response => {
+          console.log('saveAudi =>', response)
+        });
+    },
+    loadSound(track_link) {
+      console.log('this.main', track_link)
+      //Commented out some buffer conditions for now
+      // if(this.buffer) {
+      //   console.log('Resolving with buffer');
+      //   return Promise.resolve(this.buffer);
+      // } else {
+        console.log('Fetching', track_link)
+        return fetch(track_link)
+          .then(data => data.arrayBuffer())
+          .then(raw => context.decodeAudioData(raw))
+          .then(buffer => {
+            this.buffer = buffer;
+            return buffer;
+          });
+        // return new Promise((resolve, reject) => {
+        //   console.log('from audio this.$route.params.id:', this.$route.params.id)
+        //   const request = new XMLHttpRequest();
+        //   request.open('GET', '//localhost:3000/uploads/audio-12.mp3', true);
+        //   request.responseType = "arraybuffer";
+
+        //   request.onload = () => {
+        //     var Data = request.response;
+
+        //     context.decodeAudioData(Data, (buffer) => {
+        //       this.buffer = buffer;
+        //       resolve(buffer);
+        //     });
+        //   };
+        //   request.onerror = (err) => reject(err);
+        //   request.send();
+        // });
+      // }
+
+      // ES7
+      // async loadSound() {
+      //   if(!this.buffer) {
+      //     const data = await fetch('//url');
+      //     const raw = await data.arrayBuffer();
+      //     const buffer = await context.decodeAudioData(raw);
+      //     this.buffer = buffer;
+      //   }
+      //   return this.buffer;
+      // }
+
+
+    },
+    playSound(track_link) {
+      this.loadSound(track_link).then(buffer => {
+        if(source) {
+          source.stop(context.currenTime);
+          source.disconnect();
+        }
+        source = context.createBufferSource();
+        source.buffer = buffer;
+        source.connect(context.destination);
+        source.start(context.currentTime);
+        source.stop(context.currentTime + 10);
+      });
+    },
   },
   components: {
-    AudioPlayer
+    // AudioPlayer
   }
 };
 </script>
 
 <style lang="css">
+  .user-container {
+    border: 2px solid orange;
+  }
 </style>
